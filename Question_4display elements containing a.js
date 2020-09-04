2b31d88f778b167bce5af06b02c80b11bbde1470
SIDDHARTH SINGH <musicsiddharth@gmail.com>
2020-09-04 16:18:15 +0530

    Added files via upload
    Batch2 Day2 Assignment
    Question 1 
    Program to search for a particular character in a string 
    Question 2 
    Program to convert minutes to seconds 
    Question 3 
    Program to search for a element in a array of strings 
    Question 4 
    Program to display only elements containing 'a' in them from a array 
    Question 5 
    Print an array in reverse order

---
+++ Question_4display elements containing a.js
@@ -0,0 +1,8 @@
+let arr2 = Array["note","keyboard","web-cam","cpu"];
+        for(let i=0 ; i<arr2.length ; i++)
+        {
+            if(arr2[i].includes("a"))
+            {
+                console.log(arr2[i]);
+            }
+        }
